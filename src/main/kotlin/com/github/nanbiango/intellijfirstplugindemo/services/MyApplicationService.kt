e8c342a04b8094c17745f08274e770ea467487e0
GitHub Action <action@github.com>
2022-08-06 06:43:05 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nanbiango/intellijfirstplugindemo/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nanbiango.intellijfirstplugindemo.services
+
+import com.github.nanbiango.intellijfirstplugindemo.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/nanbiango/intellijfirstplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nanbiango/intellijfirstplugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nanbiango.intellijfirstplugindemo.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nanbiango.intellijfirstplugindemo.MyBundle
 
 class MyProjectService(project: Project) {
 
