3442859f76895b5854169885936bb24e7e8749ab
Michael_D <406307918@qq.com>
2022-08-18 21:37:14 +0800

    update

---
+++ src/main/kotlin/com/github/nanbiango/views/NacosSyncView.kt
@@ -0,0 +1,25 @@
+package com.github.nanbiango.views
+
+import com.intellij.openapi.ui.DialogWrapper
+import javax.swing.Box
+import javax.swing.JComponent
+
+/**
+ * Nacos同步配置视图
+ */
+class NacosSyncView : DialogWrapper(true) {
+
+    private val topBox: Box = Box.createHorizontalBox()
+    private val bottomBox: Box = Box.createHorizontalBox()
+    private val rootBox: Box = Box.createVerticalBox()
+
+    /**
+     * 创建可视化界面
+     */
+    override fun createCenterPanel(): JComponent {
+
+
+        return rootBox
+    }
+
+}
