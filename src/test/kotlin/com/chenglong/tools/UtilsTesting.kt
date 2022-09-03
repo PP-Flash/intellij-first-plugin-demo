270eac0850e58c2018c669e65a1178e19c08d1be
wangchenglong <dszz112@163.com>
2022-09-03 18:10:06 +0800

    remove nacos tools

--- src/test/kotlin/com/chenglong/tools/UtilsTesting.kt
+++ src/test/kotlin/com/chenglong/tools/UtilsTesting.kt
@@ -1,14 +1,8 @@
 package com.chenglong.tools
 
-import com.github.nanbiango.services.NacosSyncDevService
-
 /**
  * ...
  */
 fun main() {
-//    NacosSyncDevService.login()
-
-//    NacosSyncDevService.getConfig("mps-order-joom-service.yml", "dev_mps_group")
-
-    NacosSyncDevService.publishConfig("test222", "test_nacos_config.yaml")
+    
 }
